177511479567857fb36f65efa97eb0c1bc6b237d
Wangari-web-coder <joanw5837@gmail.com>
2026-04-01 08:55:19 +0300

    update

---
+++ main.kt
@@ -0,0 +1,112 @@
+//task 1
+
+    
+// var studentName:String ="Alex Morgan" //mutable
+//  val studentId:Int = 2024001  //immutable
+//  var gpa:Double = 3.75    //mutable
+//  val isEnrolled:Boolean = true    //immutable
+//  val middleInitial:Char ='j' //immutable
+ 
+
+   
+//  //task 1.2
+//  val output = "student: $studentName $middleInitial. (ID: $studentId) has GPA $gpa and is enrolled: $isEnrolled"
+
+
+//  //task 1.3
+//  var phoneNumber:String? = null 
+
+//  fun main() {
+//  print(output)
+
+//  }
+
+
+
+
+
+
+
+
+
+//  //task2.1(non-parameterized function)
+ 
+//  fun displayMenu() {
+//     println("1.Menu")
+//      println("2.View Profile")
+//      println("3.Edit settings")
+//      println("4.Logout")
+   
+//  }
+ 
+  
+  
+// fun main(){
+//     displayMenu()
+// }
+
+ 
+//  //task 2.2(parameterized function)
+//   fun calculateDiscount(price: Double, discountPercent: Double): Double{
+//       return price - (price * discountPercent/ 100)
+      
+//   }
+  
+//   fun main(){
+//       val originalprice = 100.0
+//       val discount = 10.0
+//       val discountprice = calculateDiscount(originalprice, discount)
+      
+//       println("discountprice : $discount")
+//   }
+ 
+  //task2.3(getgrade)
+  
+  
+//  fun getGrade(score: Int): String{
+//      return when (score) {
+//           in 80..100 -> "A"
+//           in 65..79 -> "B"
+//           in 50..64 -> "C"
+//           in 35..49 -> "D"
+//          else -> "F"
+//      }
+//  }
+ 
+ 
+//   fun main() {
+//       val grade = getGrade(90)
+//       println("score 90 gives grade: $grade")
+//   } 
+   
+     
+ 
+ 
+ 
+//task 2.4 (single expression)
+fun isEven(number: Int): Boolean = number % 2 == 0
+
+fun main(){
+    val score = 60
+    val number = 4
+   
+    println("the grade for $score is: {getGrade(score)}")
+    println("")
+    
+    
+}
+
+
+
+//task3(conditioanl statements)
+
+ //task 3.1(if else)
+//  fun checkTemperature(temperature: Int) {
+//     if (temperature > 30){
+//      println("Hot day")
+//     }else if (temperature in 20..30){
+//      println("Perfect weather")
+//      }else{
+//          println("cool day")
+//      }
+//  }
